732ac9fce73766c0b48f91f5664b230c71d98fa7
Dimas Neto <dimas_fcn@outlook.com>
2021-06-24 08:57:37 -0300

    home

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import './service/firebase'
+import './services/firebase';
+
+import './styles/global.scss';
 
 ReactDOM.render(
   <React.StrictMode>
